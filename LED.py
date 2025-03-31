3911b716e0ee450c75efa9e9efcbd91145ef3256
xab901 <xab901@outlook.com>
2025-03-31 04:44:11 +0100

    more comments added

--- LED.py
+++ LED.py
@@ -5,11 +5,15 @@
 
 import RPi.GPIO as GPIO
 import time
+# establish naming convention
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 print("LED on")
+# turn the pin 'on' to power the LED light
 GPIO.output(18,GPIO.HIGH)
+# wait for one second
 time.sleep(1)
 print("LED off")
+# turn the pin 'off' to shutdown supplying power to the LED
 GPIO.output(18,GPIO.LOW)
