ef6ad694586a43684505e5cb0e21f9fcfb2ad461
xab901 <xab901@outlook.com>
2025-04-02 01:44:15 +0100

    Version updated

--- Quiz1.py
+++ Quiz1.py
@@ -1,9 +1,13 @@
+""" author: Anbo Xu
+    id: 202283890035
+    date: Apr.2nd"""
+
 # NUIST Quiz Game in Python
 def quiz():
     print("Welcome to the Animal Quiz!")
     print("Answer the following questions:")
     
-    # Questions and Answers
+    # Questions and Answers, using lists to store
     questions = [
         "1.What is the largest animal on Earth?: a. Blue Whale, b. Mouse, c. Cat",
         "2. Which bird can fly backwards?: a. Cuckoo, b. Eagle, c. Hummingbird ",
@@ -16,7 +20,7 @@ def quiz():
         ]
     score = 0
 
-    # Ask questions
+    # Go through the list and check if the answers are right
     for i in range(len(questions)):
         user_answer = input(questions[i]).strip().lower()
         if user_answer == answers[i]:
